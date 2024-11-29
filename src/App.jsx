81ec59da118a9d4952a7c0cae85095db97474156
Pedro Antonio Ribeiro Pereira <pedro.arp220@gmail.com>
2024-11-29 00:19:34 -0300

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Sidebar from './components/Sidebar/Sidebar'
 import Main from './components/Main/Main'
 
 const App = () => {
+  // vai pegar as coisas do Main e da side ar e mandar pro main que vai mandar pro index.html
+// assim vai aparecer as coisas do site
   return (
     <>
       <Sidebar/>
